3cd0549b689ed1106459cf47e3efa5eac58944ea
Shion Ichikawa <shion1305@gmail.com>
2022-08-18 23:59:45 +0900

    🔨 READMEに対応するデータ項目をVuexでセットアップ

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,25 @@
-import { createStore } from 'vuex'
+import {createStore} from 'vuex'
 
 export default createStore({
   state: {
+    eventData: {
+      //それぞれのデータ項目についてはREADMEを参照
+      event_title: '',
+      event_summary: '',
+      event_description: '',
+      event_place: '',
+      event_genre_id: 0,
+      icon_filename: '',
+      org_name: '',
+      org_description: '',
+      sns_twitter: '',
+      sns_facebook: '',
+      sns_instagram: '',
+      sns_website: '',
+    }
   },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {}
 })
