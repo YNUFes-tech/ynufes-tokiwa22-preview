d8fb68f55ec7a672ab05132259e9a84b992b0099
Shion Ichikawa <shion1305@gmail.com>
2022-08-18 23:31:58 +0900

    ✨ デフォルトのレイアウトの追加

--- src/views/EventDetailView.vue
+++ src/views/EventDetailView.vue
@@ -2,9 +2,17 @@
 
 </script>
 <template>
-  <div></div>
+  <div class="root-wrapper">
+    //この中にレイアウトを記述する
+  </div>
 </template>
 
 <style scoped>
-
+.root-wrapper{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;;
+  background: #2c3e50;
+}
 </style>
