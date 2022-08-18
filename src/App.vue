4031e9a2ef17ab8e9ce6f50a8d9364cd2de2471f
Shion Ichikawa <shion1305@gmail.com>
2022-08-18 22:49:20 +0900

    ✨ Viewsを構成

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">データ入力ページ</router-link>
+    <router-link to="/eventList">一覧ページ</router-link> |
+    <router-link to="/eventDetail">詳細ページ</router-link>
   </nav>
   <router-view/>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import EventListView from '@/views/EventListView.vue'
+import EventDetailView from '@/views/EventDetailView.vue'
+import FormView from '@/views/FormView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    {
+        path: '/',
+        name: 'DataForm',
+        component: FormView
+    },
+    {
+        path: '/eventList',
+        name: 'EventListView',
+        component: EventListView
+    },
+    {
+        path: '/eventDetail',
+        name: 'EventDetailView',
+        component: EventDetailView
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
