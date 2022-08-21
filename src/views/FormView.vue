1f75dbdf4db84790a5eccd0870c8b136c1e2300b
Shion Ichikawa <shion1305@gmail.com>
2022-08-22 05:04:41 +0900

    ✨ FormViewのVuex連携・一部フィールドの追加

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -1,14 +1,46 @@
-<script setup>
+<script>
+export default {
+  data: () => ({
+    eventData: {
+      //それぞれのデータ項目についてはREADMEを参照
+      event_title: '',
+      event_summary: '',
+      event_description: '',
+      event_place_text: '',
+      event_place_id: '',
+      event_genre_id: 0,
+      icon_filename: '',
+      org_name: '',
+      org_description: '',
+      sns_twitter: '',
+      sns_facebook: '',
+      sns_instagram: '',
+      sns_website: '',
+    }
+  }),
+  created() {
+    this.eventData = this.$store.state.eventData
+  }
+}
 
 </script>
 <template>
   <div class="root-wrapper">
-    //この中にレイアウトを記述する
+    <h2>出展名</h2>
+    <input v-model="eventData.event_title">
+    <h2>企画内容(短め)</h2>
+    <input v-model="eventData.event_summary">
+    <h2>企画説明文</h2>
+    <input v-model="eventData.event_description">
+    <h2>企画場所</h2>
+    <input v-model="eventData.event_place_text">
+    <h2>ジャンル</h2>
+    <input v-model="eventData.event_genre_id">
   </div>
 </template>
 
 <style scoped>
-.root-wrapper{
+.root-wrapper {
   display: flex;
   flex-direction: column;
   align-items: center;
