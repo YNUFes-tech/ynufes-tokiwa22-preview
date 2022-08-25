c5bc1671bdaa76c93e24f211e692e0213c2dbf4c
otanisae <otani-sae-bg@ynu.jp>
2022-08-25 20:37:19 +0900

    ✨ EventDetailViewのレイアウトを追加

--- src/views/EventDetailView.vue
+++ src/views/EventDetailView.vue
@@ -3,16 +3,46 @@
 </script>
 <template>
   <div class="root-wrapper">
-    //この中にレイアウトを記述する
+    <div class="content">
+      <h2>母団体名org_name</h2>
+      <h1>出展名event_title</h1>
+      <h3>場所:event_place</h3>
+      <p>企画説明event_description</p>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.root-wrapper{
+.root-wrapper {
   display: flex;
   flex-direction: column;
-  align-items: center;
-  margin: 0 auto;;
-  background: #2c3e50;
+  /*margin: 0 auto;*/
+  /*background: #2c3e50;*/
+  background-image: url("/public/background.png");
+  background-size: cover;
+  background-attachment: fixed;
 }
+
+.content {
+  color: white;
+  margin: 300px;
+  text-align: left;
+  animation-name:fadeUpAnime;
+  animation-duration:0.5s;
+  animation-fill-mode:forwards;
+  opacity:0;
+}
+
+@keyframes fadeUpAnime{
+  from {
+    opacity: 0;
+    transform: translateY(100px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+
 </style>
