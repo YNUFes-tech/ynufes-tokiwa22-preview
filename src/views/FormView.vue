1395ee6028ba5ffa26a98212d363bb2520b6c34e
Shion Ichikawa <shion1305@gmail.com>
2022-08-26 20:22:51 +0900

    💄 フォームの入力項目をすべて整備

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -35,40 +35,73 @@ export default {
 </script>
 <template>
   <div class="root-wrapper">
-    <div class="top-description">
-      <h1>22常盤祭プレビューページへようこそ</h1>
-      <div>
-        このサイトは22常盤祭HPでの表示を確認することができます。<br>
-        フォームを記入します際にこのサイトをご活用ください。
-      </div>
+    <div class="form-item">
+      <h2>出展名</h2>
+      <ul>
+        <li>改行も適用されます。</li>
+        <li>横最大全角10文字までを想定しています。</li>
+        <li>改行を行う際は、GoogleFormに改行した状態で記入してください。</li>
+      </ul>
+      <textarea v-model="eventData.event_title"/>
     </div>
-    <h2>出展名(改行あり)</h2>
-    <textarea v-model="eventData.event_title"/>
-    <h2>企画内容(短め)</h2>
-    <input v-model="eventData.event_summary">
-    <h2>企画説明文</h2>
-    <textarea v-model="eventData.event_description"/>
-    <h2>企画場所</h2>
-    <input v-model="eventData.event_place_text">
-    <h2>ジャンル</h2>
-    <select v-model="eventData.event_genre_id">
-      <option value="0">未選択</option>
-      <option value="1">展示販売</option>
-      <option value="2">パフォーマンス</option>
-      <option value="3">ゲームスポーツ</option>
-      <option value="4">デザート</option>
-      <option value="5">鉄板・麺類</option>
-      <option value="6">ファストフード</option>
-      <option value="7">ドリンク</option>
-      <option value="8">ご飯もの</option>
-    </select>
-    <h2>アイコン</h2>
-    <h2>母団体名</h2>
-    <h2>団体説明文</h2>
-    <h2>団体のTwitterアカウント</h2>
-    <h2>団体のFacebookアカウント</h2>
-    <h2>団体のInstagramアカウント</h2>
-    <h2>団体のWebサイトリンク</h2>
+    <div class="form-item">
+      <h2>企画内容(短め)</h2>
+      <textarea v-model="eventData.event_summary"/>
+    </div>
+    <div class="form-item">
+      <h2>企画説明文</h2>
+      <ul>
+        <li>改行も適用されます。</li>
+      </ul>
+      <textarea v-model="eventData.event_description"/>
+    </div>
+    <div class="form-item">
+      <h2>企画場所</h2>
+      <input v-model="eventData.event_place_text"/>
+    </div>
+    <div class="form-item">
+      <h2>ジャンル</h2>
+      <select v-model="eventData.event_genre_id">
+        <option value="0">未選択</option>
+        <option value="1">展示販売</option>
+        <option value="2">パフォーマンス</option>
+        <option value="3">ゲームスポーツ</option>
+        <option value="4">デザート</option>
+        <option value="5">鉄板・麺類</option>
+        <option value="6">ファストフード</option>
+        <option value="7">ドリンク</option>
+        <option value="8">ご飯もの</option>
+      </select>
+    </div>
+    <div>
+      <h2>アイコン</h2>
+      <img :src="this.eventData.icon_filename" v-if="this.eventData.icon_filename">
+    </div>
+    <input type="file" ref="image" @change="processIcon"/>
+    <div class="form-item">
+      <h2>母団体名</h2>
+      <input v-model="eventData.org_name">
+    </div>
+    <div class="form-item">
+      <h2>団体説明文</h2>
+      <input v-model="eventData.org_description">
+    </div>
+    <div class="form-item">
+      <h2>団体のTwitterアカウント</h2>
+      <input v-model="eventData.sns_twitter">
+    </div>
+    <div class="form-item">
+      <h2>団体のFacebookアカウント</h2>
+      <input v-model="eventData.sns_facebook">
+    </div>
+    <div class="form-item">
+      <h2>団体のInstagramアカウント</h2>
+      <input v-model="eventData.sns_instagram">
+    </div>
+    <div class="form-item">
+      <h2>団体のWebサイトリンク</h2>
+      <input v-model="eventData.sns_website">
+    </div>
     <router-link to="/event-list">
       <div class="button">企画一覧ページをプレビュー</div>
     </router-link>
@@ -78,18 +111,54 @@ export default {
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .root-wrapper {
   display: flex;
   flex-direction: column;
   align-items: center;
   margin: 0 auto;
-  width: min(100%, 50rem);
+  width: unquote("min(100%, 50rem)");
   margin-inline: auto;
 }
 
 .top-description {
   border: #2c3e50 1px solid;
+  padding: 1rem;
 }
 
+
+ol, ul {
+  text-align: start;
+}
+
+.form-item {
+  padding: 1rem;
+  width: unquote("min(100%, 40rem)");
+
+  h2 {
+    margin: 0;
+  }
+
+  textArea {
+    width: unquote("min(100% - 2rem, 35rem)");
+    height: 5rem;
+  }
+
+  img {
+    width: 300px;
+    height: 300px;
+  }
+}
+
+.button {
+  width: fit-content;
+  padding: 0.5rem;
+  border: #0a0a8c 2px solid;
+  border-radius: 1rem;
+  margin: 1rem;
+}
+
+a {
+  text-decoration: none;
+}
 </style>
