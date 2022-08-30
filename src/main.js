11d55a69f073fc660130a57546a225d08744cc9e
Shion Ichikawa <shion1305@gmail.com>
2022-08-30 22:42:46 +0900

    ➖ vue-final-modal のライブラリ削除, #25

--- src/main.js
+++ src/main.js
@@ -2,6 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import {vfmPlugin} from "vue-final-modal";
 
-createApp(App).use(vfmPlugin).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
