236ce0b07d87b9942a3ee910a10bdaf142643e1d
Shion Ichikawa <shion1305@gmail.com>
2022-09-18 13:30:52 +0900

    ✨ すべてのページをLazyLoadingに変更

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,38 @@
 import {createRouter, createWebHistory} from 'vue-router'
-import EventListView from '@/views/EventListView.vue'
-import EventDetailView from '@/views/EventDetailView.vue'
-import FormView from '@/views/FormView.vue'
-import CustomEventListView from '@/views/CustomEventListView.vue'
-import CustomEventDetailView from '@/views/CustomEventDetailView.vue'
 
 const routes = [
-    {
-        path: '/',
-        name: 'DataForm',
-        component: FormView
-    },
-    {
-        path: '/event-list',
-        name: 'EventListView',
-        component: EventListView
-    },
-    {
-        path: '/event-detail',
-        name: 'EventDetailView',
-        component: EventDetailView
-    },
-    {
-        path: `/event-list/:id`,
-        name:`CustomEventListView`,
-        component: CustomEventListView,
-        props:(route) => ({...route.params,EventId: route.params.id})
-    },
-    {
-        path: '/event-detail/:id',
-        name:'CustomEventDetailView',
-        component: CustomEventDetailView,
-        props:(route) => ({...route.params,EventId: route.params.id})
-    }
+  {
+    path: '/',
+    name: 'DataForm',
+    component: () => import(/* webpackChunkName: "test-formview" */ '../views/FormView.vue'),
+  },
+  {
+    path: '/event-list',
+    name: 'EventListView',
+    component: () => import(/* webpackChunkName: "test-eventlistview" */ '../views/EventListView.vue'),
+  },
+  {
+    path: '/event-detail',
+    name: 'EventDetailView',
+    component: () => import(/* webpackChunkName: "test-eventdetailview" */ '../views/EventDetailView.vue'),
+  },
+  {
+    path: `/event-list/:id`,
+    name: `CustomEventListView`,
+    component: () => import(/* webpackChunkName: "customized-eventlistview" */ '../views/CustomEventListView.vue'),
+    props: (route) => ({...route.params, EventId: route.params.id})
+  },
+  {
+    path: '/event-detail/:id',
+    name: 'CustomEventDetailView',
+    component: () => import(/* webpackChunkName: "customized-eventdetailview" */ '../views/CustomEventDetailView.vue'),
+    props: (route) => ({...route.params, EventId: route.params.id})
+  }
 ]
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes
+  history: createWebHistory(process.env.BASE_URL),
+  routes
 })
 
 export default router
